59bda923b0e8b7151f4e44439301c9f17150e49c
Codart-7 <iconmoa@gmail.com>
2023-05-09 15:54:31 +0100

    basic employees setup

---
+++ src/controller/v0/Employees/EmployeeController.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+
+class Employees {
+    public async login(req: Request, res: Response) {}
+}
+
+
+const employees = new Employees();
+export default employees;
